3896f22ccdb042a8ca6b57716c29f3f973ea9def
Patrick Jordan <mail@ptj.dev>
2023-07-08 04:35:19 -0600

    Welcome new members

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import os
+import json
 import discord
 from discord.ext import commands
-import json
 
 # Load config data from file
 with open('config.json') as f:
     config = json.load(f)
 
+# Define intents object
+intents = discord.Intents.all()
+
 # Create instance of bot
-bot = commands.Bot(command_prefix=config["prefix"])
+bot = commands.Bot(command_prefix=config["prefix"], intents=intents)
 
 # Bot event
 @bot.event
@@ -18,6 +22,13 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send('Bong!')
+
+# Welcome message
+@bot.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name="general")
+    if channel:
+        await channel.send(f"Welcome to the server, {member.mention}")
 
 # Run bot
 bot.run(config["token"])
